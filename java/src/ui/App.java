affd6bdb2aa83d49652fdfd922a25591aaef4117
Merebe <merebe2012@gmail.com>
2024-03-18 16:57:03 +0100

    Test changes in App.java

--- java/src/ui/App.java
+++ java/src/ui/App.java
@@ -8,8 +8,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         ScanRepository scanRepository = new ScanRepository();
         ScanInteractor scanInteractor = new ScanInteractor(scanRepository);
-        ScanController scanController = new ScanController(scanInteractor, 1);
+        ScanController scanController = new ScanController(scanInteractor, 0);
 
-        System.out.println("Hello, World!");
+        System.out.println("Premier scan : \n" + scanController.onGetScan(1).getScan());
     }
 }
